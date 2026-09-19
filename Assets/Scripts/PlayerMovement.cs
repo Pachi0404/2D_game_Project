45dde3bd034f3b267a8337ccc30e14e00914218b
Steven Harvie <stevenharvie4@gmail.com>
2026-09-19 19:22:13 +1000

    Scripts and Objects
    created scripts and objects defined who is player, added in rigidbody and colliders

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 10;
+    [SerializeField] private float playerJumpForce = 2;
+
+    public bool isPlayerGrounded;
+    
+    private Rigidbody2D rb;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isPlayerGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, playerJumpForce);
+        }
+    }
+    private void FixedUpdate()
+    {
+        isPlayerGrounded = Physics.Raycast(
+            transform.position, 
+            Vector2.down,
+            out RaycastHit hit,
+            0.1f);
+
+        if (hit.collider != null && hit.collider.CompareTag("Ground"))
+        {
+            isPlayerGrounded = true;
+        }
+        else
+        {
+            isPlayerGrounded = false;
+        }
+        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        
+        rb.linearVelocity = new Vector2(horizontalInput * playerSpeed, rb.linearVelocity.y);
+    }
+}
---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    public float frequency = 1f;
+    
+    private Vector3 startPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        MoveWithCosine();
+    }
+
+    void MoveWithCosine()
+    {
+        float xOffset = movementSpeed * Mathf.Cos(Time.time * frequency);
+    
+        transform.position = new Vector3(startPosition.x + xOffset, startPosition.y, startPosition.z);
+    }
+}
